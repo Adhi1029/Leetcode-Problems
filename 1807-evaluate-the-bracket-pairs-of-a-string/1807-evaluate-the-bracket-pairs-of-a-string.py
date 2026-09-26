26e7e67d728c29c65fc0143ba8c18725da86ef05
Adithyaraj <adithyaraj1029@gmail.com>
2026-09-26 16:01:31 +0530

    Time: 37 ms (91.28%), Space: 52 MB (13.4%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -1,4 +1,4 @@
 class Solution:
-    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        d = dict(knowledge)
-        return re.sub(r"\((\w+)\)", lambda m: d.get(m[1], "?"), s)
+    def evaluate(self, s: str, d: List[List[str]]) -> str:
+        return s.replace('(','{d[').replace(')',']}').format(d=defaultdict(lambda:'?',d))
+        return re.sub(r'\((\w+)\)',lambda m,d=dict(d):d.get(m[1],'?'),s)
